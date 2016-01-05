e71d929a647a4ac9420200987972e5f535b5620f
Cameron Tacklind <cameron@tacklind.com>
2016-01-05 15:23:48 -0800

    Cleanup

--- gccGuard.cpp
+++ gccGuard.cpp
@@ -7,6 +7,12 @@
 
 #include "gccGuard.h"
 
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-void __cxa_guard_abort (__guard *) {};
+int __cxa_guard_acquire(__guard *g) {
+ return !*(char *)(g);
+}
+
+void __cxa_guard_release (__guard *g) {
+ *(char *)g = 1;
+}
+
+void __cxa_guard_abort (__guard *) {}
