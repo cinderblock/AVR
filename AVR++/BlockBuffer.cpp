b2a2c9d486cacccb009fb8814b7cb090ec965dd3
Cameron Tacklind <cameron@tacklind.com>
2016-06-23 15:07:49 -0700

    Add BlockBuffer implementation

---
+++ AVR++/BlockBuffer.cpp
@@ -0,0 +1,74 @@
+/* 
+ * File:   BlockBuffer.cpp
+ * Author: Cameron
+ * 
+ * Created on June 23, 2016, 1:32 PM
+ */
+
+#include <avr/interrupt.h>
+
+#include "BlockBuffer.h"
+
+template<u1 BlockSize, u1 Blocks, bool readInterrupt, bool writeInterrupt>
+u1* BlockBuffer<BlockSize, Blocks, readInterrupt, writeInterrupt>::getReadBuffer() {
+	if (currentRead == 0xff) return nullptr;
+	return buffers[currentRead];
+}
+
+template<u1 BlockSize, u1 Blocks, bool readInterrupt, bool writeInterrupt>
+u1* BlockBuffer<BlockSize, Blocks, readInterrupt, writeInterrupt>::getWriteBuffer() {
+	if (currentWrite = 0xff) return nullptr;
+	return buffers[currentWrite];
+}
+
+template<u1 BlockSize, u1 Blocks, bool readInterrupt, bool writeInterrupt>
+void BlockBuffer<BlockSize, Blocks, readInterrupt, writeInterrupt>::markNewestBuffer() {
+	if (!hasRoom())
+		return;
+
+	if (writeInterrupt) cli();
+
+	u1 const curr = currentWrite;
+	u1 nextWrite = curr;
+
+	nextWrite++;
+
+	if (nextWrite > Blocks)
+		nextWrite = 0;
+
+	if (currentRead == 0xff)
+		currentRead = curr;
+
+	if (nextWrite == currentRead)
+		nextWrite = 0xff;
+
+	currentWrite = nextWrite;
+
+	if (writeInterrupt) sei();
+}
+
+template<u1 BlockSize, u1 Blocks, bool readInterrupt, bool writeInterrupt>
+void BlockBuffer<BlockSize, Blocks, readInterrupt, writeInterrupt>::reserveNewestBufferForReading() {
+	if (!isNewData())
+		return;
+
+	if (readInterrupt) cli();
+
+	u1 const curr = currentRead;
+	u1 nextRead = curr;
+
+	nextRead++;
+
+	if (nextRead > Blocks)
+		nextRead = 0;
+
+	if (currentWrite == 0xff)
+		currentWrite = curr;
+
+	if (nextRead == currentWrite)
+		nextRead = 0xff;
+
+	currentRead = nextRead;
+
+	if (readInterrupt) sei();
+}
---
+++ AVR++/BlockBuffer.h
@@ -0,0 +1,77 @@
+/* 
+ * File:   BlockBuffer.h
+ * Author: Cameron
+ *
+ * Created on June 23, 2016, 1:32 PM
+ */
+
+#ifndef BLOCKBUFFER_H
+#define	BLOCKBUFFER_H
+
+#include "basicTypes.h"
+
+using namespace AVR;
+
+template <u1 BlockSize, u1 Blocks, bool readInterrupt, bool writeInterrupt = !readInterrupt>
+class BlockBuffer {
+
+ u1 buffers[Blocks][BlockSize];
+ /**
+  * Index of which buffer we're currently writing to. A special value of 0xff
+  * is used to indicate no room is available
+  */
+ u1 currentWrite;
+
+ /**
+  * Index of which buffer we're currently reading from. A special value of 0xff
+  * is used to indicate no data is available
+  */
+ u1 currentRead;
+
+public:
+ inline BlockBuffer() : currentRead(0xff), currentWrite(0) {
+   static_assert(Blocks > 0, "Must use a positive, non-zero number of blocks");
+   static_assert(Blocks != 255, "Maximum 254 blocks");
+ }
+
+ /**
+  * Reserve the newest block for reading
+  * @return
+  */
+ void reserveNewestBufferForReading();
+
+ /**
+  * Mark the buffer the writer is using as the newest, indicating that data is
+  * available and allowing the reader to access the next block
+  * @return
+  */
+ void markNewestBuffer();
+
+ /**
+  * Check if there is new data available for the reader side
+  * @return
+  */
+ inline bool isNewData() {return currentRead < Blocks;}
+
+ /**
+  * Check if there is room to write another block to the buffer
+  * @return
+  */
+ inline bool hasRoom() {return currentWrite < Blocks;}
+
+ /**
+  * Get the buffer that should be written to
+  * @return
+  */
+ u1 * getWriteBuffer();
+
+ /**
+  * Get the buffer that should be read from
+  * @return
+  */
+ u1 * getReadBuffer();
+
+};
+
+#endif	/* BLOCKBUFFER_H */
+
