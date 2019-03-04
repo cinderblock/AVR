2738ae9aa1f1692014e403c4737e525fbaa50a5b
Cameron Tacklind <cameron@tacklind.com>
2019-03-03 23:19:03 -0800

    Add ScanningADC implementation

---
+++ AVR++/ScanningADC.cpp
@@ -0,0 +1,50 @@
+
+
+#include "ScanningADC.hpp"
+
+using namespace AVR;
+using namespace Basic;
+
+// Start at -1 so that first run starts at 0
+template <u1 N> u1 ScanningADC<N>::current = -1;
+
+template <u1 N> void ScanningADC<N>::init() {
+
+  // Enable "High Speed Mode". Doesn't actually seem to do anything.
+  ADCSRB = (1 << ADHSM);
+
+  // Enable ADC with interrupt and suggested prescaler
+  // *ADC::ControlStatusRegisterA = {true};
+  ADCSRA = 0b10011000 | (u1)ADC::suggestedPrescaler;
+
+  if (selectNext()) {
+    inputs[current].mux.select();
+
+    ADC::startConversion();
+  }
+}
+
+template <u1 N> bool ScanningADC<N>::selectNext() {
+  auto const start = current;
+  do {
+    current++;
+    if (current >= N)
+      current = 0;
+
+    if (start == current)
+      return false;
+  } while (!inputs[current].handle);
+
+  return true;
+}
+
+template <u1 N> void ScanningADC<N>::interrupt() {
+  inputs[current].handle();
+
+  if (!selectNext())
+    return;
+
+  inputs[current].mux.select();
+
+  ADC::startConversion();
+}
---
+++ AVR++/ScanningADC.hpp
@@ -0,0 +1,55 @@
+
+#include "ADC.hpp"
+#include "avr/interrupt.h"
+
+ISR(ADC_vect, ISR_NOBLOCK);
+
+namespace AVR {
+using namespace Basic;
+
+template <u1 N> class ScanningADC {
+
+  friend void ::ADC_vect();
+
+public:
+  /**
+   * A function that will be called when the most recent data is available in ADC register.
+   *
+   * Executed in interrupt context, but interrupts are on.
+   */
+  typedef void (*Handler)();
+
+  /**
+   * The ADC MUX value and a function to call when the conversion is complete
+   */
+  typedef struct {
+    ADC::RegularInput mux;
+    Handler handle;
+  } Input;
+
+private:
+  static Input inputs[N];
+
+  /**
+   * Current index in the table.
+   */
+  static u1 current;
+
+  /**
+   * Call this from ISR(ADC_vect). Something like:
+   * ```C++
+   * #include <AVR++/ScanningADC.cpp>
+   * template <u1 N> typename ScanningADC<N>::Input ScanningADC<N>::inputs[] = {...};
+   * template class ScanningADC<numberOfScannedAnalogInputs>;
+   * ISR(ADC_vect) { ScanningADC<numberOfScannedAnalogInputs>::interrupt(); }
+   * ```
+   */
+  static void interrupt();
+
+  static bool selectNext();
+
+public:
+  static void init();
+};
+
+} // namespace AVR
