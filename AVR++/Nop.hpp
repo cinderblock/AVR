fe8c911c9433eb46c6826c36b8fe2f85fd138b82
Cameron Tacklind <cameron@tacklind.com>
2021-12-16 03:17:03 -0800

    Initial WS2812 implementation

---
+++ AVR++/Nop.hpp
@@ -0,0 +1,19 @@
+
+// VS Code complains without this. GCC doesn't need it.
+extern "C" void __builtin_avr_delay_cycles(long unsigned int);
+
+namespace AVR {
+static inline void nopCycles(unsigned cycles) {
+  // GCC provides an excellent builtin for this.
+  // It's a lot faster than the loops below but it's not always available.
+#ifdef __BUILTIN_AVR_DELAY_CYCLES
+  __builtin_avr_delay_cycles(cycles);
+#else
+  if (cycles & 1)
+    asm volatile("nop");
+  cycles /= 2;
+  while (cycles--)
+    asm volatile("rjmp .");
+#endif
+}
+} // namespace AVR
---
+++ AVR++/WS2812.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "IOpin.hpp"
+
+namespace AVR {
+
+template <class port, u1 pin, bool HandleInterrupts = true, unsigned resetMicroseconds = 300,
+          bool InvertedOutput = false, bool LittleEndian = false>
+class WS2812 : protected Output<port, pin, InvertedOutput> {
+protected:
+  using Output<port, pin>::on;
+  using Output<port, pin>::off;
+
+  /**
+   * @brief Shift an array of bytes out the specified pin using the WS2812 protocol.
+   *
+   * Does not include reset pulse.
+   *
+   * @param bytes the bytes to send
+   * @param length the number of bytes to send
+   */
+  static void sendBytes(u1 const *bytes, u2 length) __attribute__((naked));
+
+public:
+  struct RGB {
+    u1 g;
+    u1 r;
+    u1 b;
+  };
+  struct RGBW {
+    u1 g;
+    u1 r;
+    u1 b;
+    u1 w;
+  };
+
+  static void setLEDs(RGB const *leds, u2 pixels);
+  static void setLEDs(RGBW const *leds, u2 pixels);
+};
+
+}; // namespace AVR
