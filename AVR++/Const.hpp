8a71a2cc5a76ac9b809cbbbaaceb9e9212e16232
Cameron Tacklind <cameron@tacklind.com>
2021-12-23 22:17:38 -0800

    Update types

--- AVR++/Const.hpp
+++ AVR++/Const.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-template <typename T> static constexpr int const_round(T f) { return f > 0.0 ? int(f + 0.5) : int(f - 0.5); }
-template <typename T> static constexpr T max(T a, T b) { return a > b ? a : b; }
-template <typename T> static constexpr T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline constexpr int const_round(T f) { return f > 0.0 ? int(f + 0.5) : int(f - 0.5); }
+template <typename T> inline constexpr T max(T a, T b) { return a > b ? a : b; }
+template <typename T> inline constexpr T min(T a, T b) { return a < b ? a : b; }
