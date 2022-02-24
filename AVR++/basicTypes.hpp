b1e2e2854213cebefc77739ba0d2b4a8f8e2d6b8
Cameron Tacklind <cameron@tacklind.com>
2022-02-24 12:29:46 -0800

    Add 3 byte types

--- AVR++/basicTypes.hpp
+++ AVR++/basicTypes.hpp
@@ -12,13 +12,15 @@
 
 namespace Basic {
 
-typedef uint8_t  u1;
+typedef uint8_t u1;
 typedef uint16_t u2;
+typedef __uint24 u3;
 typedef uint32_t u4;
 typedef uint64_t u8;
 
-typedef int8_t  s1;
+typedef int8_t s1;
 typedef int16_t s2;
+typedef __int24 s3;
 typedef int32_t s4;
 typedef int64_t s8;
 
